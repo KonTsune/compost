cbef3ca8e02b7205570b1afa2ea8a79654708ff6
tsune <kon.tsunemasa@gimail.com>
2022-11-11 16:57:00 +0900

    温度テスト

--- test/test2.py
+++ test/test2.py
@@ -1,23 +1,33 @@
-import RPi.GPIO as GPIO
-import time
+import os
+import glob
+from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.OUT)
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-PWM = 13
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
-GPIO.setup(PWM, GPIO.OUT)
-
-p = GPIO.PWM(PWM, 200)
-p.start(0)
-
-
-for i in range(10, 101, 10):
-	p.ChangeDutyCycle(i)
-	print(i)
-	time.sleep(2)
-
-p.ChangeDutyCycle(0)
-print('done')
-
-GPIO.cleanup(PWM)
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+		
+try:
+    while True:
+        print(read_temp())
+        sleep(3)
+except KeyboardInterrupt:
+    pass
