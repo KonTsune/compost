8faa6b84a5bbb3333cb3fff05c278992063cdf98
tsune <kon.tsunemasa@gimail.com>
2022-12-05 16:03:15 +0900

    5階の温度保存ができた

--- code/sensing.py
+++ code/sensing.py
@@ -1,6 +1,8 @@
 import glob
 import os
 import subprocess
+import pickle
+
 from time import sleep
 from gpiozero import MCP3002
 
@@ -41,7 +43,41 @@ def read_hum():
     return hum
 
 # データ送信と情報表示
+'''
 result = subprocess.run(['python3', '/home/compost/compost/code/hard-data-transmission/main.py', 'add', '-i', '{}'.format(read_temp()), '{}'.format(read_hum())])
 print(result)
+'''
+temp = read_temp()
+hum = read_hum()
 
-print("温度:", read_temp(), "土壌水分:", read_hum())
+print("温度:", temp, "土壌水分:", hum)
+
+# 温度保存
+btemp = [temp]
+
+filepath = "/home/compost/compost/code/pic.bin"
+print(os.path.exists(filepath))
+if os.path.exists(filepath) == True:
+    with open('pic.bin', 'rb') as p:
+        btemp = pickle.load(p)
+        print("前回までの温度", btemp)
+        ave = sum(btemp) / len(btemp)
+        print("前回までの平均", ave)
+        if len(btemp) == 5:
+            btemp.pop(0)
+            btemp.append(temp)
+            print(1,btemp)
+        else:
+            btemp.append(temp)
+            print(2,btemp)
+    with open('pic.bin', 'wb') as p:
+        pickle.dump(btemp, p)
+        print("書き込んだ")
+else:
+    with open('pic.bin', 'wb') as p:
+        pickle.dump(btemp, p)
+        print("ぴっく作った", btemp)
+
+# 以下撹拌条件設定
+if temp <= ave-5:
+    print("撹拌")
